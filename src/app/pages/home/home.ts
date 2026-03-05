e9212ea27f444ba80c5b42859d509929236a5491
gwanga <wangageremi725@gmail.com>
2026-03-05 20:43:23 +0300

    add the blog section to the home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -4,18 +4,19 @@ import { FeaturedPropertiesComponent } from '../../featured-properties/featured-
 import { WhoWeAreComponent } from '../../who-we-are/who-we-are';
 import { HowItWorksComponent } from '../../how-it-works/how-it-works';
 import { ExploreCitiesComponent } from '../../explore-cities/explore-cities';
-
+import { BlogInsightsComponent } from '../../blog-insights/blog-insights';
 
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent,FeaturedPropertiesComponent,WhoWeAreComponent, HowItWorksComponent, ExploreCitiesComponent],
+  imports: [HeroComponent,FeaturedPropertiesComponent,WhoWeAreComponent, HowItWorksComponent, ExploreCitiesComponent, BlogInsightsComponent],
   template: `
   <app-hero></app-hero> 
   <app-featured-properties></app-featured-properties>
   <app-who-we-are></app-who-we-are>
   <app-how-it-works></app-how-it-works>
-  <app-explore-cities></app-explore-cities>`,
+  <app-explore-cities></app-explore-cities>
+  <app-blog-insights></app-blog-insights>`,
   styleUrl: './home.css',
 })
 export class Home {}
