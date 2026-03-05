17a5177dc15d960a68365f3bbe2038bfe84716c0
gwanga <wangageremi725@gmail.com>
2026-03-05 20:42:08 +0300

    add explre cities section to the main home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,15 +3,19 @@ import { HeroComponent } from '../../hero/hero';
 import { FeaturedPropertiesComponent } from '../../featured-properties/featured-properties';
 import { WhoWeAreComponent } from '../../who-we-are/who-we-are';
 import { HowItWorksComponent } from '../../how-it-works/how-it-works';
+import { ExploreCitiesComponent } from '../../explore-cities/explore-cities';
+
+
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent,FeaturedPropertiesComponent,WhoWeAreComponent, HowItWorksComponent],
-  template: ` 
+  imports: [HeroComponent,FeaturedPropertiesComponent,WhoWeAreComponent, HowItWorksComponent, ExploreCitiesComponent],
+  template: `
   <app-hero></app-hero> 
   <app-featured-properties></app-featured-properties>
   <app-who-we-are></app-who-we-are>
-  <app-how-it-works></app-how-it-works>`,
+  <app-how-it-works></app-how-it-works>
+  <app-explore-cities></app-explore-cities>`,
   styleUrl: './home.css',
 })
 export class Home {}
