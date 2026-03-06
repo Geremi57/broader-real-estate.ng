af3dcb8b30461f548ab602c067de1eb086f881fb
gwanga <wangageremi725@gmail.com>
2026-03-06 17:30:27 +0300

    import property service

--- src/app/pages/properties/properties.ts
+++ src/app/pages/properties/properties.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { PropertyService } from '../../services/property';
 
 @Component({
   selector: 'app-properties',
