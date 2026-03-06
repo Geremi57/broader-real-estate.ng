e960bd484779dfd1626cec3c510c0f4bcc591670
gwanga <wangageremi725@gmail.com>
2026-03-06 17:31:39 +0300

    create central interface to store data from api

---
+++ src/app/models/property.ts
@@ -0,0 +1,11 @@
+export interface Props {
+  id: number
+  title: string
+  slug: string
+  price: number
+  location: string
+  bedrooms: number
+  bathrooms: number
+  image: string
+  description: string
+}
