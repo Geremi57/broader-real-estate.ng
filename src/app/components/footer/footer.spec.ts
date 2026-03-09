c632c8e36a8f33545d2250a97847ea69128b1959
gwanga <wangageremi725@gmail.com>
2026-03-09 13:42:32 +0300

    fix all .spec files with providerouter pkg

--- src/app/components/footer/footer.spec.ts
+++ src/app/components/footer/footer.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
 import { FooterComponent } from './footer';
 
 describe('Footer', () => {
@@ -9,6 +9,9 @@ describe('Footer', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [FooterComponent],
+      providers: [
+        provideRouter([])
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(FooterComponent);
--- src/app/components/navbar/navbar.spec.ts
+++ src/app/components/navbar/navbar.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';  // Add this import
 import { NavbarComponent } from './navbar';
 
 describe('Navbar', () => {
@@ -9,6 +9,9 @@ describe('Navbar', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NavbarComponent],
+      providers: [
+        provideRouter([])
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(NavbarComponent);
